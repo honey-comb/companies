43a70796077ad353e927d4bb6a921bbaec62596b
Jevgenij Dmitrijev <jd@interactivesolutions.lt>
2018-01-23 20:32:10 +0200

    Added company controller

---
+++ src/Http/Controllers/Admin/HCCompanyController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace HoneyComb\Companies\Http\Controllers\Admin;
+
+use HoneyComb\Companies\Services\HCCompanyService;
+use HoneyComb\Core\Helpers\HCFrontendResponse;
+use HoneyComb\Core\Http\Controllers\HCBaseController;
+use Illuminate\Database\Connection;
+
+class HCCompanyController extends HCBaseController
+{
+    /**
+     * @var HCCompanyService
+     */
+    private $service;
+
+    /**
+     * @var Connection
+     */
+    private $connection;
+
+    /**
+     * @var HCFrontendResponse
+     */
+    private $response;
+
+    public function __construct(Connection $connection, HCFrontendResponse $response, HCCompanyService $service)
+    {
+        $this->service = $service;
+        $this->connection = $connection;
+        $this->response = $response;
+    }
+
