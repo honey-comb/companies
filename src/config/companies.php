acc74c92bd85d091b97b0d04f7acbf8886d9a8c2
Jevgenij Dmitrijev <jd@interactivesolutions.lt>
2018-01-23 20:32:50 +0200

    Added companies configuration

---
+++ src/config/companies.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    "companies" =>
+        [
+            "lt" =>
+                [
+                    "url" => "http://www.rekvizitai.lt/api-xml/",
+                    "apiKey" => env('HC_COMPANIES_LT_API_KEY'),
+                    "clientId" => env('HC_COMPANIES_LT_CLIENT_ID'),
+                ],
+        ]
+];
