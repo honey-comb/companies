c35524a7e2862ce8bfcfe459203edb8dd2703ba9
Jevgenij Dmitrijev <jd@interactivesolutions.lt>
2018-01-29 10:06:56 +0200

    Company Controller improve

--- src/Http/Controllers/Admin/HCCompanyController.php
+++ src/Http/Controllers/Admin/HCCompanyController.php
@@ -1,33 +1,38 @@
 <?php
 
-namespace HoneyComb\Companies\Http\Controllers\Admin;
+    namespace HoneyComb\Companies\Http\Controllers\Admin;
 
-use HoneyComb\Companies\Services\HCCompanyService;
-use HoneyComb\Core\Helpers\HCFrontendResponse;
-use HoneyComb\Core\Http\Controllers\HCBaseController;
-use Illuminate\Database\Connection;
+    use HoneyComb\Companies\Services\HCCompanyService;
+    use HoneyComb\Core\Helpers\HCFrontendResponse;
+    use HoneyComb\Core\Http\Controllers\HCBaseController;
+    use Illuminate\Database\Connection;
 
-class HCCompanyController extends HCBaseController
-{
-    /**
-     * @var HCCompanyService
-     */
-    private $service;
-
-    /**
-     * @var Connection
-     */
-    private $connection;
-
-    /**
-     * @var HCFrontendResponse
-     */
-    private $response;
-
-    public function __construct(Connection $connection, HCFrontendResponse $response, HCCompanyService $service)
+    class HCCompanyController extends HCBaseController
     {
-        $this->service = $service;
-        $this->connection = $connection;
-        $this->response = $response;
-    }
+        /**
+         * @var HCCompanyService
+         */
+        private $service;
 
+        /**
+         * @var Connection
+         */
+        private $connection;
+
+        /**
+         * @var HCFrontendResponse
+         */
+        private $response;
+
+        public function __construct(Connection $connection, HCFrontendResponse $response, HCCompanyService $service)
+        {
+            $this->service = $service;
+            $this->connection = $connection;
+            $this->response = $response;
+        }
+
+        public function findByCode (string $code)
+        {
+            return $this->service->findByCode($code, "lt");
+        }
+    }
